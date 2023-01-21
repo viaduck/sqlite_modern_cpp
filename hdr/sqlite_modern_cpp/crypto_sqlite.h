b1bbfebb422f0bf9eff746a2a068cdca38914e54
Alexandros Roussos <thealexroussos@gmail.com>
2023-01-21 17:45:27 +0100

    Adapted to crypto_sqlite rename

--- hdr/sqlite_modern_cpp/crypto_sqlite.h
+++ hdr/sqlite_modern_cpp/crypto_sqlite.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#include <cryptosqlite/cryptosqlite.h>
+#include <crypto_sqlite/crypto_sqlite.h>
 #include "../sqlite_modern_cpp.h"
 
 namespace sqlite {
-	struct cryptosqlite_config : public sqlite_config {
+	struct crypto_sqlite_config : public sqlite_config {
 		std::string key;
 	};
 
-	class cryptosqlite_database : public database {
+	class crypto_sqlite_database : public database {
 	public:
-		cryptosqlite_database(const std::string &db_name, const cryptosqlite_config &config):
+		crypto_sqlite_database(const std::string &db_name, const crypto_sqlite_config &config):
 				database(db_name, prepare_config(config)) {
 
 			if(auto ret = sqlite3_key(_db.get(), config.key.data(), config.key.size()))
@@ -21,7 +21,7 @@ namespace sqlite {
 
 	private:
 		// workaround preparing the sqlite3_open before the base constructor calls it
-		const cryptosqlite_config &prepare_config(const cryptosqlite_config &config) {
+		const crypto_sqlite_config &prepare_config(const crypto_sqlite_config &config) {
 			sqlite3_prepare_open_encrypted(config.key.data(), config.key.size());
 			return config;
 		}
